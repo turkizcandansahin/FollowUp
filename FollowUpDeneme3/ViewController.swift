58b5fc6bf1b91756ec3c3b09038e62987b3187b4
Turkiz candan Sahin <turkizcandansahin@gmail.com>
2023-08-04 16:43:20 +0300

    Initial Commit

---
+++ FollowUpDeneme3/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  FollowUpDeneme3
+//
+//  Created by Türkiz Candan Şahin on 3.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var ButtonCollection: [UIButton]!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        ButtonCollection.forEach { UIButton in
+            UIButton.layer.cornerRadius = 10
+            UIButton.layer.borderWidth = 1
+            UIButton.layer.borderColor = CGColor(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+    @IBAction func signInButton(_ sender: Any) {
+    }
+    
+}
+
