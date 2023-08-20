0edd70f78028ea68eb8293105359249951f7789f
Turkiz candan Sahin <turkizcandansahin@gmail.com>
2023-08-20 15:42:24 +0300

    onboarding VC is done

--- FollowUpDeneme3/Storyboards+VCs/OnBoardingVC.swift
+++ FollowUpDeneme3/Storyboards+VCs/OnBoardingVC.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnBoardingVC: UIViewController {
 
     
     @IBOutlet var ButtonCollection: [UIButton]!
     
+    @IBOutlet weak var gradientView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +20,17 @@ class ViewController: UIViewController {
         ButtonCollection.forEach { UIButton in
             UIButton.layer.cornerRadius = 10
             UIButton.layer.borderWidth = 1
-            UIButton.layer.borderColor = CGColor(red: 1, green: 1, blue: 1, alpha: 1)
+            UIButton.layer.borderColor = CGColor(red: 1, green: 1, blue: 1, alpha: 0.5)
         }
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = gradientView.bounds
+        gradientLayer.colors = [UIColor.opaqueSeparator.cgColor,UIColor.black.cgColor]
+        gradientLayer.locations = [0, 0.5]
+        gradientView.layer.addSublayer(gradientLayer)
     }
+    
+    
     
     @IBAction func signInButton(_ sender: Any) {
     }
